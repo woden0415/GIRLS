2160032d563196b1c00b1d332fb718077f86c838
wang_donga <wang_donga@Ctrip.com>
2018-08-04 15:58:59 +0800

    支持图片反防盗链，第一版封版本

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var imgsRouter = require('./routes/imgs');
+var imgsUrlRouter = require('./routes/imgsUrl');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/imgs', imgsRouter)
+app.use('/imgUrl', imgsUrlRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/imgsUrl.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: wang_donga@Ctrip.com
+ * @Date: 2018-07-12 00:09:48
+ * @Description: 图片接口
+ */
+
+var express = require('express');
+var http = require('http')
+var fs = require('fs')
+var request = require('request')
+
+
+var router = express.Router();
+
+let {
+  funcSelectImgs
+} = require("../old/database")
+// console.log(funcSelectImgs )
+// const querystring = require('querystring');
+const {
+  URL,
+  URLSearchParams
+} = require('url');
+
+router.get('/url', function (req, res, next) {
+  let url = ''
+  let str = req.url;
+  str = str.slice(9)
+  console.log('str', str);
+  const myURL = new URL(str);
+
+  var options = {
+    url: str,
+    headers: {
+      'Referer': 'http://m.27270.com/'
+    }
+  };
+  request(options)
+    .on('error', function (err) {
+      console.log(err)
+    })
+    .pipe(res);
+});
+
+module.exports = router;
