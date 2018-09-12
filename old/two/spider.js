bee6c51bc9a3dd510d0b25999b71b5622b3a3d0c
wang_donga <wang_donga@Ctrip.com>
2018-09-12 22:38:50 +0800

    优化

--- old/two/spider.js
+++ old/two/spider.js
@@ -6,10 +6,15 @@
 var superagent = require('superagent')
 var charset = require("superagent-charset");
 var cheerio = require('cheerio')
+var userAgents = require('./util/userAgent')
+var proxyAgents = require('./util/proxyAgent')
 
+require('superagent-proxy')(superagent)
 charset(superagent);
 var fs = require('fs')
-
+// var proxy = proxyAgents[parseInt(Math.random() * proxyAgents.length)];
+var proxy = 'http://transfer.moguproxy.com:9001';
+var appKey = 'bDhHajFKa21xMUtSd0l5TjpIdVgwdmlzSEp4cGVBOTJX';
 const Throttle = require('superagent-throttle');
 let throttle = new Throttle({
   active: true,     // set false to pause queue
@@ -25,21 +30,17 @@ class Label {
 
   getAllLabelsInfos() {
     let url = 'http://www.27270.com/tag/'
-    let time = 0;
     let arrList = this.getAllLabelsLink(url);
     arrList.then((result) => {
       console.log('result.length', result.length);
       result.map((urlItem, index, arr) => {
         this.getLabelDetail(urlItem).then((aa)=>{
           (function(url, i, a){
-            console.log(`请求链接: ${i}    ----     ${url}`);
-            if (i === 0) {
-              fs.appendFileSync(`${__dirname}/label.js`, `[${JSON.stringify(aa, null, 2)},\n`)
-            } else if (i === arr.length - 1) {
-              fs.appendFileSync(`${__dirname}/label.js`, `${JSON.stringify(aa, null, 2)}]`)
-            } else {
-              fs.appendFileSync(`${__dirname}/label.js`, `${JSON.stringify(aa, null, 2)},\n`)
-            }
+            fs.appendFile(`${__dirname}/label.js`, `${JSON.stringify(aa, null, 2)},`, function(err) {
+              if (err) {
+                throw err
+              }
+            })
           })(urlItem, index, arr)
         }).catch((e)=>{console.log(e);})
       })
@@ -59,7 +60,7 @@ class Label {
    */
   getAllLabelsLink(listUrl) {
     // let arrGirls = [3, 5, 6, 11, 12, 20, 21];
-    let arrGirls = [3]
+    let arrGirls = [5]
     let promiseA = new Promise((resolve) => {
       let newArr = [];
       superagent
@@ -88,7 +89,6 @@ class Label {
    * @description 获取该标签的详情数据
    * @param {String} url 该标签的url
    */
-
    getLabelDetail(url) {
     return new Promise((resolve) => {
       superagent
@@ -106,6 +106,7 @@ class Label {
             let $ = cheerio.load(res.text);
             let labelPages = $('.TagPage').find('a').length !== 0 ? $('.TagPage').find('a').last().attr('href').split('')[$('.TagPage').find('a').last().attr('href').indexOf('.html') - 1] : 1
             let objLabelInfo = {
+              labelId: generatorRandomString(20),
               labelName: $('.Tag_Title_Gs').text(),
               labelDesc: $('.TagTop_box_Gs').children('p').text(),
               labelOther: [$('.list_tag').children('a').text()],
@@ -118,6 +119,29 @@ class Label {
         })
     })
   }
+
+  // 删除多余重复标签
+  deleteRepeat() {
+    getFile('label.js').then((arr) => {
+      console.log(arr.length);
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = i+1; j < arr.length; j++) {
+          if (arr[i].labelLink === arr[j].labelLink) {
+            // arr.splice(i, 1);
+          }
+        }
+      }
+      console.log(arr.length);
+      // fs.appendFile(`${__dirname}/album2.js`, `${JSON.stringify(arr, null, 2)},`, (err) => {
+      //   if (err) {
+      //     throw err
+      //   }
+      // })
+    })
+  }
+
+
+
 }
 
 /**
@@ -135,7 +159,7 @@ class Album{
 
   main() {
     let arrLabel = [];
-    this.getAllLabelsInfos1().then((arr) => {
+    getFile('label.js').then((arr) => {
       // 循环当前数组，取出url，发起请求，获取当前末页 limitLen， 循环limitLen，获取每一页的ablums
       let arrAlbums = [];
 
@@ -166,45 +190,24 @@ class Album{
                 arrA.map((aIndex, aItem) => {
                   // console.log(`${labelItem.labelName} --- ${i}页 --- ${$(aItem).attr('title')}`);
                   objAlbum = {
-                    label: `${labelItem.labelName}`,
+                    albumId: generatorRandomString(20),
+                    labelId: `${labelItem.labelId}`,
                     url: `${$(aItem).attr('href')}`,
                     title: `${$(aItem).attr('title')}`,  // 标题
                     desc: '',   // 描述
                     coverUrl: `${$(aItem).children('img').attr('src')}` // 封面链接
                   }
-                  if (labelIndex === 0 && i === 0 && aIndex === 0) {
-                    fs.appendFileSync(`${__dirname}/album.js`, `[${JSON.stringify(objAlbum, null, 2)},\n`)
-                  } else if (labelIndex === labelArr - 1 && i === labelItem.pageSize - 1 && aIndex === numAlength - 1) {
-                    fs.appendFileSync(`${__dirname}/album.js`, `${JSON.stringify(objAlbum, null, 2)}]`)
-                  } else {
-                    fs.appendFileSync(`${__dirname}/album.js`, `${JSON.stringify(objAlbum, null, 2)},\n`)
-                  }
+                  fs.appendFile(`${__dirname}/album.js`, `${JSON.stringify(objAlbum, null, 2)},`, (err) => {
+                    if (err) {
+                      throw err
+                    }
+                  })
                 })
               }
             })
         }
       })
     })
-  }
-
-
-  // @todo 读取所有label的labelLink
-  // @todo 进入label页面，获取页数， 循环页数，获取当前页面的专辑 coverUrl，标注好当前的label
-
-  /**
-   * @description 返回存储的所有标签信息
-   */
-  getAllLabelsInfos1() {
-    let promise1 = new Promise((resolve) => {
-      fs.readFile(`${__dirname}/label.js`, (err, data)=> {
-        if (err) {
-          console.log(err);
-          return 0
-        }
-        resolve(JSON.parse(data))
-      })
-    })
-    return promise1
   }
 
   // 获取专辑的描述内容，和总页数
@@ -217,29 +220,62 @@ class Album{
       }
       arrAlbum = JSON.parse(data);
       arrAlbum.map((albumItem, albumIndex, albumArr) => {
-        superagent
-          .get(albumItem.url)
-          .use(throttle.plugin())
-          .set({
-            'Cookie': 'Hm_lvt_63a864f136a45557b3e0cbce07b7e572=1532689632Hm_lpvt_63a864f136a45557b3e0cbce07b7e572=1534031600',
-            'Referer': 'http://www.27270.com/'
-          })
-          .charset('gbk')
-          .end(function (err, res) {
-            if (err) {
-              console.log('err.status', err)
-            } else {
-              // console.log(`${albumItem.label} --- ${albumItem.title}`)
-              let $ = cheerio.load(res.text);
-              Object.assign(albumItem, {
-                desc: $('.articleV4Desc').children('p').text(),
-                coverUrl: $('#picBody').find('img').attr('src'),
-                pageSize: $('#pageinfo').attr('pageinfo')
-              });
-              fs.appendFileSync(`${__dirname}/album1.js`, `${JSON.stringify(albumItem, null, 2)},\n`)
-            }
-          })
+        // if (arrLeft.indexOf(albumItem.url) > -1) {
+          superagent
+            .get(albumItem.url)
+            .use(throttle.plugin())
+            .set({
+              'Cookie': 'Hm_lvt_63a864f136a45557b3e0cbce07b7e572=1532689632Hm_lpvt_63a864f136a45557b3e0cbce07b7e572=1534031600',
+              'Referer': 'http://www.27270.com/'
+            })
+            .charset('gbk')
+            .end(function (err, res) {
+              if (err) {
+                // console.log('err.status', albumItem.url)
+                appendFile('log/albumerror.log', albumItem.url + '\n');
+              } else {
+                console.log('albumItem.url :', albumItem.url);
+                let $ = cheerio.load(res.text);
+                Object.assign(albumItem, {
+                  desc: $('.articleV4Desc').children('p').text() || '',
+                  coverUrl: $('#picBody').find('img').attr('src') || '',
+                  pageSize: $('#pageinfo').attr('pageinfo') || '0'
+                });
+                fs.appendFile(`${__dirname}/album1.js`, `${JSON.stringify(albumItem, null, 2)},\n`, (err) => {
+                  if (err) {
+                    throw err;
+                  }
+                })
+              }
+            })
+        // }
       })
+    })
+  }
+
+  // 排除专辑里重复的内容
+  deleteRepeat() {
+    let index = 0
+    getFile('album1.js').then((arr) => {
+      for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+          if (arr[i].url === arr[j].url) {
+            console.log('url: ', arr[i].url)
+            arr.splice(j, 1)
+            index++
+          }
+        }
+      }
+      console.log('index :', index);
+      if (index !== 0) {
+        fs.writeFile(`${__dirname}/album.js`, `${JSON.stringify(arr, null, 2)},`, (err) => {
+          if (err) {
+            throw err
+          }
+        })
+      } else {
+        console.log('没有重复值')
+      }
     })
   }
 }
@@ -259,7 +295,7 @@ class Img {
     let arrAlbum = [];
     fs.readFile(`${__dirname}/album1.js`, (err, data) => {
       if (err) {
-        console.log('errAllAlbum', err)
+        console.log('errAllAlbum', )
         return 0
       }
       arrAlbum = JSON.parse(data);
@@ -271,26 +307,36 @@ class Img {
           } else {
             url = `${albumItem.url.slice(0, albumItem.url.indexOf('.html'))}_${i+1}.html`
           }
+          let userAgent = userAgents[parseInt(Math.random() * userAgents.length)];
           superagent
-          .get(url)
-          .use(throttle.plugin())
-          .set({
-            'Cookie': 'Hm_lvt_63a864f136a45557b3e0cbce07b7e572=1532689632Hm_lpvt_63a864f136a45557b3e0cbce07b7e572=1534031600',
-            'Referer': 'http://www.27270.com/'
-          })
-          .charset('gbk')
-          .end(function (err, res) {
-            if (err) {
-              console.log('err.status', err)
-            } else {
-              let $ = cheerio.load(res.text);
-              let objImg = {
-                album: albumItem.title,
-                imgUrl: $('#picBody').find('img').attr('src'),
+            .get(url)
+            .set({ 'User-Agent': userAgent })
+            .proxy(proxy)
+            .use(throttle.plugin())
+            .set({
+              'Cookie': 'Hm_lvt_63a864f136a45557b3e0cbce07b7e572=1532689632Hm_lpvt_63a864f136a45557b3e0cbce07b7e572=1534031600',
+              'Referer': 'http://www.27270.com/',
+              'Proxy-Authorization': `Basic ${appKey}`
+            })
+            .charset('gbk')
+            .end(function (err, res) {
+              if (err) {
+                appendFile('log/imgerror.log', url)
+              } else {
+                let $ = cheerio.load(res.text);
+                let objImg = {
+                  albumId: albumItem.albumId,
+                  album: albumItem.title,
+                  imgId: generatorRandomString(20),
+                  imgUrl: $('#picBody').find('img').attr('src'),
+                }
+                fs.appendFile(`${__dirname}/imgs.js`, `${JSON.stringify(objImg, null, 2)},\n`, (err) => {
+                  if (err) {
+                    throw err;
+                  }
+                })
               }
-              fs.appendFileSync(`${__dirname}/imgs.js`, `${JSON.stringify(objImg, null, 2)},\n`)
-            }
-          })
+            })
         }
       })
     })
@@ -298,14 +344,52 @@ class Img {
 
 }
 
+// 工具函数
+// 生成随机id
+function generatorRandomString(length) {
+  let arrChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let charLen = arrChar.length;
+  let str = '';
+  for (let i = 0; i < length; i++) {
+    str += arrChar[Math.floor(Math.random() * charLen)]
+  }
+  return str;
+};
+
+// 获取文件内容
+function getFile(filename) {
+  let promise1 = new Promise((resolve) => {
+    fs.readFile(`${__dirname}/${filename}`, (err, data)=> {
+      if (err) {
+        console.log(err);
+        return 0
+      }
+      resolve(JSON.parse(data))
+    })
+  })
+  return promise1
+}
+
+// 向某个文件添加 append
+function appendFile(filename, data) {
+  fs.appendFile(`${__dirname}/${filename}`, `${data}\n`, (err) => {
+    if (err) {
+      throw err;
+      console.log(data);
+    }
+  })
+}
 
 
 // let label = new Label();
 // label.getAllLabelsInfos()
+// label.deleteRepeat()
 
-// let album = new Album();
+let album = new Album();
 // album.main()
-// album.getAlbumInfo()
+album.getAlbumInfo()
+// album.deleteRepeat() // 去重
+
 
 // let img = new Img();
 // img.getAllAlbum();
